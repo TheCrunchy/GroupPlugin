f50f426c03104f2009e821fc66ebb09c7276e7db
TheCrunchy <crunchycamsta@gmail.com>
2021-06-02 02:57:35 +0100

    Starting the discord stuff

---
+++ AlliancesPlugin/Alliances/DiscordStuff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlliancesPlugin.Alliances
+{
+    public class DiscordStuff
+    {
+
+        //do the discord shit in here
+
+        public static void SendMessageToDiscord(string message)
+        {
+
+        }
+
+        public static void SendDiscordToIngame(string message)
+        {
+
+        }
+
+    }
+}
