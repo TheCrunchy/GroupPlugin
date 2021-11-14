850726db18d88471580fb63c2e5dacae82337fa9
TheCrunchy <crunchycamsta@gmail.com>
2021-11-14 01:06:36 +0000

    Update AlliancePlugin.cs

--- AlliancesPlugin/AlliancePlugin.cs
+++ AlliancesPlugin/AlliancePlugin.cs
@@ -98,7 +98,7 @@ namespace AlliancesPlugin
             }
 
         }
-
+        
         public static void BackupGridMethod(List<MyObjectBuilder_CubeGrid> Grids, long User)
         {
             try
