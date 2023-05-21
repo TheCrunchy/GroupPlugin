2794f924820a0359eb7970821a1d1752984818d2
TheCrunchy <crunchycamsta@gmail.com>
2023-05-21 20:29:28 +0100

    secondary logic

--- AlliancesPlugin/Territory Version 2/Interfaces/ISecondaryLogic.cs
+++ AlliancesPlugin/Territory Version 2/Interfaces/ISecondaryLogic.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlliancesPlugin.Alliances.NewTerritories;
 
 namespace AlliancesPlugin.Territory_Version_2.Interfaces
 {
     public interface ISecondaryLogic
     {
         //return if the cap has succeeded, and if so an object of who now owns it
-        Task DoSecondaryLogic();
+        Task DoSecondaryLogic(ICapLogic point, Territory territory);
         DateTime NextLoop { get; set; }
         int SecondsBetweenLoops { get; set; }
     }
