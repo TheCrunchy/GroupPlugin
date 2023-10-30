47235b8898164f7ef37bcc60576d5e22e6640f5c
TheCrunchy <crunchycamsta@gmail.com>
2023-10-30 18:31:54 +0000

    remove unused stuff

--- TerritoryPlugin/Territories/Models/Territory.cs
+++ TerritoryPlugin/Territories/Models/Territory.cs
@@ -13,13 +13,8 @@ namespace Territory.Territory_Version_2.Models
         public string WorldName = "default";
         public Guid Id = System.Guid.NewGuid();
         public string Name = "Example";
-        public int Radius = 50000;
         public bool Enabled = false;
-        public bool ForcesPvP = true;
         public List<ICapLogic> CapturePoints = new List<ICapLogic>();
-        public string EntryMessage = "You are in {name} Territory";
-        public string ControlledMessage = "Controlled by {alliance}";
-        public string ExitMessage = "You have left {name} Territory";
         public Vector3D Position;
         public string DiscordWebhook = "https://discord.com/api/webhooks/1110180136118132827/DpKhjeIFUxwJqw8r1piKs0fnJ4HZCg4EcHiSCvzlHT0szKptgSoZNVHym7KdN8FjxKbc";
         public string EmbedColorString = "5763719";
