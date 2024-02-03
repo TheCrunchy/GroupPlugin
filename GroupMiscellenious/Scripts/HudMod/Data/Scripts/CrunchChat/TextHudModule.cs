5a85ed405239cdf8f537f78f8cd7c7d332fc5764
TheCrunchy <crunchycamsta@gmail.com>
2024-02-03 13:48:23 +0000

    hud stuff

---
+++ GroupMiscellenious/Scripts/HudMod/Data/Scripts/CrunchChat/TextHudModule.cs
@@ -0,0 +1,135 @@
+﻿using Draygo.API;
+using Sandbox.Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VRageMath;
+using BlendTypeEnum = VRageRender.MyBillboard.BlendTypeEnum;
+
+namespace Crunch
+{
+    public class TextHudModule
+    {
+        public TextHudModule() { }
+
+        HudAPIv2 HUD_Base;
+
+        HudAPIv2.HUDMessage HUD_ChatStatus;
+        StringBuilder ChatStatusText = new StringBuilder("");
+
+        HudAPIv2.HUDMessage HUD_ChatInfo;
+        StringBuilder ChatInfoText = new StringBuilder("");
+
+        HudAPIv2.HUDMessage HUD_AreaName;
+        StringBuilder AreaNameText = new StringBuilder("");
+
+        HudAPIv2.HUDMessage HUD_AreaPvPEnabled;
+        StringBuilder AreaPvPEnabledText = new StringBuilder("");
+
+        HudAPIv2.HUDMessage HUD_AreaHeader;
+        StringBuilder AreaHeaderText = new StringBuilder("");
+
+
+
+        public bool HudInit = false;
+
+        private void HUD_Init_Complete()
+        {
+            // Init AreaHeader line
+            HUD_AreaHeader = new HudAPIv2.HUDMessage(AreaHeaderText, new Vector2D(0.75, 0.95), null, -1, 1.25, true, false, null, BlendTypeEnum.PostPP, "white");
+
+            // Init AreaName line
+            HUD_AreaName = new HudAPIv2.HUDMessage(AreaNameText, new Vector2D(0.75, 0.90), null, -1, 1, true, false, null, BlendTypeEnum.PostPP, "white");
+
+            // Init AreaPvPEnabled line
+            HUD_AreaPvPEnabled = new HudAPIv2.HUDMessage(AreaPvPEnabledText, new Vector2D(0.75, 0.85), null, -1, 1, true, false, null, BlendTypeEnum.PostPP, "white");
+
+            // Init status line
+            HUD_ChatStatus = new HudAPIv2.HUDMessage(ChatStatusText, new Vector2D(-0.7, -0.60), null, -1, 1, true, false, null, BlendTypeEnum.PostPP, "white");
+
+            // Init info line
+            HUD_ChatInfo = new HudAPIv2.HUDMessage(ChatInfoText, new Vector2D(-0.7, -0.65), null, -1, 1, true, false, null, BlendTypeEnum.PostPP, "white");
+
+
+            SetChatStatus(true);
+            SetAreaHeader(true);
+            SetAreaName();
+            SetChatInfo(false);
+            HudInit = true;
+  
+        }
+
+        public void Init()
+        {
+            HUD_Base = new HudAPIv2(HUD_Init_Complete);
+        }
+		
+		public void SetAreaNotVisible(){
+			
+            HUD_AreaHeader.Visible = false;
+            HUD_AreaName.Visible = false;
+            HUD_AreaPvPEnabled.Visible = false;
+		}
+		
+		public void SetAreaVisible(){
+            HUD_AreaHeader.Visible = true;
+            HUD_AreaName.Visible = true;
+            HUD_AreaPvPEnabled.Visible = true;
+			
+		}
+        public void SetAreaHeader(bool check)
+        {
+            AreaHeaderText.Clear();
+            if (check)
+            {
+                HUD_AreaHeader.InitialColor = Color.Cyan;
+                AreaHeaderText.Append("PvP Area Info");
+            }
+
+            HUD_AreaHeader.Visible = true;
+            HUD_AreaName.Visible = true;
+            HUD_AreaPvPEnabled.Visible = true;
+
+        }
+
+        public void SetAreaName(string name = "")
+        {
+            AreaNameText.Clear();
+            if (!name.Equals(""))
+            {
+                HUD_AreaName.InitialColor = Color.White;
+                AreaNameText.Append($"Area: {name}");
+                HUD_AreaHeader.Visible = true;
+                HUD_AreaName.Visible = true;
+                HUD_AreaPvPEnabled.Visible = true;
+            }
+            else
+            {
+                HUD_AreaHeader.Visible = false;
+                HUD_AreaName.Visible = false;
+                HUD_AreaPvPEnabled.Visible = false;
+            }
+
+        }
+
+        public void SetAreaPvPEnabled(string owner)
+        {
+            AreaPvPEnabledText.Clear();
+            HUD_AreaPvPEnabled.InitialColor = Color.Red;
+            AreaPvPEnabledText.Append($"Owner: {owner}");
+
+        }
+
+        public void SetChatStatus(bool status)
+        {
+            HUD_ChatStatus.Visible = false;
+			HUD_ChatInfo.Visible = false;
+        }
+
+        public void SetChatInfo(bool status)
+        {
+            HUD_ChatStatus.Visible = false;
+            HUD_ChatInfo.Visible = false;
+        }
+    }
+}
---
+++ GroupMiscellenious/Scripts/TerritoryHudScript.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrunchGroup;
+using CrunchGroup.Models;
+using CrunchGroup.Territories.CapLogics;
+using CrunchGroup.Territories.Models;
+using CrunchGroup.Territories.PointOwners;
+using ProtoBuf;
+using Sandbox.Game.World;
+using Sandbox.ModAPI;
+using Torch.Managers.PatchManager;
+using VRage.Game.ModAPI;
+using VRageMath;
+
+namespace GroupMiscellenious.Scripts
+{
+    [PatchShim]
+    public class TerritoryHudScript
+    {
+        private static int ticks;
+        public static DateTime NextSendTerritory = DateTime.Now.AddMinutes(5);
+        public static void UpdateExample()
+        {
+            ticks++;
+            if (ticks % 128 == 0)
+            {
+                if (DateTime.Now > NextSendTerritory)
+                {
+                    foreach (var player in MySession.Static.Players.GetOnlinePlayers())
+                    {
+                        SendPlayerTerritories(player.Id.SteamId);
+                    }
+                }
+            }
+
+        }
+
+        public static void Patch(PatchContext ctx)
+        {
+            Core.UpdateCycle += UpdateExample;
+            MyAPIGateway.Multiplayer.RegisterSecureMessageHandler(8544, ReceiveModMessage);
+        }
+
+        public static void SendPlayerHudStatus(ulong steamPlayerId)
+        {
+
+            var message = new BoolStatus
+            {
+                Enabled = true
+            };
+
+
+            var statusM = MyAPIGateway.Utilities.SerializeToBinary(message);
+            var modmessage = new ModMessage()
+            {
+                Type = "HudStatus",
+                Member = statusM
+            };
+
+            var bytes = MyAPIGateway.Utilities.SerializeToBinary(modmessage);
+
+            var binaryData = MyAPIGateway.Utilities.SerializeToBinary(modmessage);
+            MyAPIGateway.Multiplayer.SendMessageTo(8544, binaryData, steamPlayerId);
+        }
+
+        public static void SendPlayerTerritories(ulong steamPlayerId)
+        {
+            var id = Core.GetIdentityByNameOrId(steamPlayerId.ToString());
+            if (id == null)
+            {
+                return;
+            }
+            IMyFaction playerFac = MySession.Static.Factions.GetPlayerFaction(id.IdentityId);
+
+            var message = new PlayerDataPvP
+            {
+                PvPAreas = new List<PvPArea>()
+            };
+
+
+
+            foreach (var territory in Core.Territories)
+            {
+                var owner = "Unowned";
+                switch (territory.Value.Owner)
+                {
+                    case FactionPointOwner faction:
+                        MyFaction fac = (MyFaction)faction.GetOwner();
+                        owner = fac.Tag;
+                        break;
+                    case GroupPointOwner group:
+                        var groupT = (Group)group.GetOwner();
+                        owner = groupT.GroupName;
+                        break;
+                }
+
+                message.PvPAreas.Add(new PvPArea()
+                {
+                    Name = territory.Value.Name ?? "PvP Area",
+                    Position = territory.Value.Position,
+                    Distance = territory.Value.RadiusDistance,
+                    OwnerName = owner
+                });
+
+                foreach (var capture in territory.Value.CapturePoints)
+                {
+                    owner = "Unowned";
+                    switch (capture.PointOwner)
+                    {
+                        case FactionPointOwner faction:
+                            MyFaction fac = (MyFaction)faction.GetOwner();
+                            owner = fac.Tag;
+                            break;
+                        case GroupPointOwner group:
+                            var groupT = (Group)group.GetOwner();
+                            owner = groupT.GroupName;
+                            break;
+                    }
+                    if (capture is FactionGridCapLogic gridcap)
+                    {
+                        message.PvPAreas.Add(new PvPArea()
+                        {
+                            Name = gridcap.PointName,
+                            Position = gridcap.GPSofPoint,
+                            Distance = gridcap.CaptureRadius,
+                            OwnerName = owner
+                        });
+                    }
+                    else
+                    {
+                        message.PvPAreas.Add(new PvPArea()
+                        {
+                            Name = capture.PointName,
+                            Position = capture.GetPointsLocationIfSet(),
+                            Distance = 10000,
+                            OwnerName = owner
+                        });
+                    }
+                }
+            }
+
+            var statusM = MyAPIGateway.Utilities.SerializeToBinary(message);
+            var modmessage = new ModMessage()
+            {
+                Type = "PvPAreas",
+                Member = statusM
+            };
+
+            var bytes = MyAPIGateway.Utilities.SerializeToBinary(modmessage);
+
+            var binaryData = MyAPIGateway.Utilities.SerializeToBinary(modmessage);
+            MyAPIGateway.Multiplayer.SendMessageTo(8544, binaryData, steamPlayerId);
+        }
+        public static void ReceiveModMessage(ushort handlerId, byte[] data, ulong steamID, bool fromServer)
+        {
+            var Data = MyAPIGateway.Utilities.SerializeFromBinary<ModMessage>(data);
+
+            switch (Data.Type)
+            {
+                case "DataRequest":
+                    var request = MyAPIGateway.Utilities.SerializeFromBinary<DataRequest>(Data.Member);
+                    switch (request.DataType)
+                    {
+                        case "Territory":
+                            SendPlayerTerritories(request.SteamId);
+                            //  AlliancePlugin.Log.Info($"Sending territories to {request.SteamId}");
+                            break;
+                        case "Hud":
+                            //    AlliancePlugin.Log.Info($"Sending war status to {request.SteamId}");
+                            SendPlayerHudStatus(request.SteamId);
+                            break;
+                        case "Chat":
+                            //    AlliancePlugin.Log.Info($"Sending war status to {request.SteamId}");
+                            //  AllianceChat.AddOrRemoveToChat(request.SteamId);
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+
+    [ProtoContract]
+    public class DataRequest
+    {
+        [ProtoMember(1)]
+        public ulong SteamId;
+
+        [ProtoMember(2)]
+        public string DataType;
+    }
+
+    [ProtoContract]
+    public class BoolStatus
+    {
+        [ProtoMember(1)]
+        public bool Enabled;
+    }
+
+    [ProtoContract]
+    public class ModMessage
+    {
+        [ProtoMember(1)]
+        public string Type;
+
+        [ProtoMember(2)]
+        public byte[] Member;
+    }
+    [ProtoContract]
+    public class PlayerDataPvP
+    {
+        [ProtoMember(1)]
+        public List<PvPArea> PvPAreas;
+    }
+
+    [ProtoContract]
+    public class PvPArea
+    {
+        [ProtoMember(1)]
+        public Vector3D Position;
+
+        [ProtoMember(2)]
+        public float Distance;
+
+        [ProtoMember(3)]
+        public string Name;
+
+        [ProtoMember(4)]
+        public string OwnerName;
+    }
+}
