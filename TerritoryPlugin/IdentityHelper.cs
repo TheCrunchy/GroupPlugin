9f237e1a523b882eede004e6f3eebb4cb55d2bc9
TheCrunchy <crunchycamsta@gmail.com>
2024-03-11 19:01:40 +0000

    grid manager that returns grids after pasting

---
+++ TerritoryPlugin/IdentityHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sandbox.Game.World;
+
+namespace CrunchGroup
+{
+    public static class IdentityHelper
+    {
+        public static MyIdentity GetIdentityByNameOrId(string playerNameOrSteamId)
+        {
+            foreach (var identity in MySession.Static.Players.GetAllIdentities())
+            {
+                if (identity.DisplayName == playerNameOrSteamId)
+                    return identity;
+                if (ulong.TryParse(playerNameOrSteamId, out ulong steamId))
+                {
+                    ulong id = MySession.Static.Players.TryGetSteamId(identity.IdentityId);
+                    if (id == steamId)
+                        return identity;
+                    if (identity.IdentityId == (long)steamId)
+                        return identity;
+                }
+
+            }
+            return null;
+        }
+
+    }
+}
+
