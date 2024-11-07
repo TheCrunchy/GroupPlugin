bdb4640f73b39311d80854d9ac27c8ff40c91eb1
TheCrunchy <crunchycamsta@gmail.com>
2024-11-07 20:19:21 +0000

    .

---
+++ GroupMiscellenious/Scripts/NexusDirectReference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrunchGroup;
+using NGPlugin;
+using NGPlugin.SyncComponents;
+using Torch.Commands;
+using Torch.Commands.Permissions;
+using VRage.Game.ModAPI;
+
+namespace GroupMiscellenious.Scripts
+{
+    public class NexusDirectReference : CommandModule
+    {
+        [Command("ngtest", "test command")]
+        [Permission(MyPromoteLevel.Admin)]
+        public void Reload()
+        {
+            var item = SyncBase.GetSync<OnlineServerSync>().GetAllOnlinePlayers();
+            Context.Respond($"{item.Count} players online");
+        }
+    }
+}
