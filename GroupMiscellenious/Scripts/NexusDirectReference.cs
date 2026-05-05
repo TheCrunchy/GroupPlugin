0e663975c06f6f7f30c1c3a990005abeed95e9ff
TheCrunchy <crunchycamsta@gmail.com>
2026-05-05 10:21:24 +0100

    207 fix

--- GroupMiscellenious/Scripts/NexusDirectReference.cs
+++ GroupMiscellenious/Scripts/NexusDirectReference.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CrunchGroup;
-using NGPlugin;
-using NGPlugin.SyncComponents;
-using Torch.Commands;
-using Torch.Commands.Permissions;
-using VRage.Game.ModAPI;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using CrunchGroup;
+//using NGPlugin;
+//using NGPlugin.SyncComponents;
+//using Torch.Commands;
+//using Torch.Commands.Permissions;
+//using VRage.Game.ModAPI;
 
-namespace GroupMiscellenious.Scripts
-{
-    public class NexusDirectReference : CommandModule
-    {
-        [Command("ngtest", "test command")]
-        [Permission(MyPromoteLevel.Admin)]
-        public void Reload()
-        {
-            var item = SyncBase.GetSync<OnlineServerSync>().GetAllOnlinePlayers();
-            Context.Respond($"{item.Count} players online");
-        }
-    }
-}
+//namespace GroupMiscellenious.Scripts
+//{
+//    public class NexusDirectReference : CommandModule
+//    {
+//        [Command("ngtest", "test command")]
+//        [Permission(MyPromoteLevel.Admin)]
+//        public void Reload()
+//        {
+//            var item = SyncBase.GetSync<OnlineServerSync>().GetAllOnlinePlayers();
+//            Context.Respond($"{item.Count} players online");
+//        }
+//    }
+//}
